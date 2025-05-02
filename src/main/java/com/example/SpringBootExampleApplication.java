00c6d908c6ee7146cf56761f744af071e0b2f14c
irumi <irumiabeywickrama@gmail.com>
2025-05-02 19:29:33 +0530

    Create person api, service, and model

--- src/main/java/com/example/SpringBootExampleApplication.java
+++ src/main/java/com/example/SpringBootExampleApplication.java
@@ -2,12 +2,16 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExampleApplication.class, args);
 	}
+
 
 }
