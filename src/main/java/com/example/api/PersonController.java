57bce06d6131eab55298720a4f00c117f9c4664f
irumi <irumiabeywickrama@gmail.com>
2025-05-02 19:37:22 +0530

    complete api connection

--- src/main/java/com/example/api/PersonController.java
+++ src/main/java/com/example/api/PersonController.java
@@ -2,10 +2,12 @@ package com.example.api;
 
 import com.example.model.Person;
 import com.example.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class PersonController {
     private final PersonService personService;
 
+    @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
