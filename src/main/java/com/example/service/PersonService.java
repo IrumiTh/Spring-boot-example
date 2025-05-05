9d748f13427c13125bde8442e4764fcab9ee8d26
irumi <irumiabeywickrama@gmail.com>
2025-05-05 08:54:34 +0530

    complete delete and update person from database function

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -30,4 +31,6 @@ public class PersonService {
     public Optional<Person> getPersonById(UUID id){
         return personDao.selectPersonById(id);
     }
+
+
 }
