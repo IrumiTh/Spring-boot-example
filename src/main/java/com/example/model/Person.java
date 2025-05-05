a27fd511be07ff27aaa01071a2b71e39f89e4aa7
irumi <irumiabeywickrama@gmail.com>
2025-05-05 09:32:17 +0530

    set name can not be null in person model

--- src/main/java/com/example/model/Person.java
+++ src/main/java/com/example/model/Person.java
@@ -1,11 +1,17 @@
 package com.example.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank; // For Spring Boot 3+
+
+
+
+
 
 import java.util.UUID;
 
 public class Person {
     private final UUID id;
+    @NotBlank
     private final String name;
 
     public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name){
